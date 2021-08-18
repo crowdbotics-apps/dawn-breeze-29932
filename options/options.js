c1e243b2f3e4d09040f2c39e3f3a2fee9f53b229
Crowdbotics <team@crowdbotics.com>
2021-08-18 22:31:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dawn_breeze_29932Identifier",
+  url: "https://dawn_breeze_29932Identifier.botics.co",
+  api: "https://dawn_breeze_29932Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
